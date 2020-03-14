6475f245dfa49edb77ba0a9a8b3174224747d1b0
rixo <rixo@rixo.fr>
2020-03-14 22:56:08 +0100

    clean

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ export { default as Render } from './Render.svelte'
 export { default as Register } from './Register.svelte'
 
 export { default as Svench } from './Svench.svelte'
-
-// private
-export { setContext } from './util'
