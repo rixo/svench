3195c26cbc610ac33646d696f84f42da75790321
rixo <rixo@rixo.fr>
2020-03-14 01:41:57 +0100

    clean

--- example/src/main.js
+++ example/src/main.js
@@ -2,23 +2,6 @@ import {Svench} from 'svench'
 
 import { routes } from '@sveltech/routify/tmp/routes'
 
-// // const app = document.createElement('svench-app')
-// document.body.innerHTML = '<svench-app></svench-app>'
-// const app = document.body.querySelector('svench-app')
-//
-// // document.body.appendChild(app)
-//
-// // console.log('set', routes)
-// setTimeout(() => {
-//   console.log(app)
-//   app.$set({routes})
-//   // app.routes = routes
-// }
-// , 500)
-//
-// // window.Svench = target => new Svench({ target, props: { routes } })
-console.log(routes)
-
 const app = new Svench({
   target: document.body,
   props: {
