75e7c4008d7b78813d31df9baf2d8063434a4c28
rixo <rixo@rixo.fr>
2020-03-16 22:48:43 +0100

    renders registering components off screen to prevent DOM pollution

--- src/RegisterRoutes.svelte
+++ src/RegisterRoutes.svelte
@@ -19,8 +19,26 @@
 
   // ensure removed pages are reflected (on HMR update)
   $: trimPages(pageRoutes)
-</script>
 
-{#each pageRoutes as route (route.path)}
-  <Register {route} {pages} />
-{/each}
+  let registers = []
+
+  // renders registering components off screen to prevent DOM polution from
+  // components that contains something else than <View /> components
+  const offScreen = document.createElement('div')
+
+  const registerRoutes = routes => {
+    for (const cmp of registers) {
+      cmp.$destroy()
+    }
+    registers = []
+    for (const route of routes) {
+      const cmp = new Register({
+        target: offScreen,
+        props: { route, pages },
+      })
+      registers.push(cmp)
+    }
+  }
+
+  $: registerRoutes(pageRoutes)
+</script>
