79aef0c66728e951adaeba229974d502ee1862af
rixo <rixo@rixo.fr>
2020-04-22 16:33:29 +0200

    clean

--- src/AppContext.svelte
+++ src/AppContext.svelte
@@ -1,15 +1,10 @@
 <script>
-  import { writable } from 'svelte/store'
-  import { getContext, updateContext } from './util.js'
+  import { getContext } from './util.js'
 
   export let App
   export let focus
 
   const { router, options, tree, extras } = getContext()
-
-  const currentRoute = writable(null)
-
-  updateContext({ currentRoute })
 </script>
 
 <svelte:component this={App} {options} {tree} {router} {focus} extras={$extras}>
