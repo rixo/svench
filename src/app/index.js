b4ab21a821b1811c27d3ab111c66682491c5301a
rixo <rixo@rixo.fr>
2020-05-05 03:52:02 +0200

    embed prism in app

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,5 @@
 export { default as App } from './App.svelte'
 export { default as ViewBox } from './ViewBox.svelte'
 export { default as RenderBox } from './RenderBox.svelte'
+
+import '../../prism.js'
