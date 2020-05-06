7eb8f681701ef94f85ade678b9d031454c964f86
rixo <rixo@rixo.fr>
2020-05-06 02:48:12 +0200

    add split, reduce, & peep

--- src/util.js
+++ src/util.js
@@ -16,14 +16,93 @@ export const updateContext = transform =>
       : { ...getContext(), ...transform }
   )
 
-export const pipe = (...fns) => x => fns.reduce((f, g) => g(f), x)
-
 export const noop = () => {}
+
+export const identity = x => x
+
+// export const pipe = (...fns) => x => fns.reduce((f, g) => g(f), x)
+
+export const pipe = (...fns) => {
+  if (typeof fns[0] === 'string') {
+    const prop = fns.shift()
+    const o = {
+      [prop]: x => fns.reduce((f, g) => g(f), x),
+    }
+    return o[prop]
+  }
+  return x => fns.reduce((f, g) => g(f), x)
+}
+{
+  let nesting = 0
+  let padding = 0
+
+  pipe.peep = (...fns) => {
+    const _log = (prefix, char) => x => {
+      // eslint-disable-next-line no-console
+      console.log(
+        '  '.repeat(nesting) + prefix.padStart(padding - nesting * 2, char),
+        x
+      )
+      return x
+    }
+
+    return pipe(
+      typeof fns[0] === 'string' ? fns[0] : identity,
+      // nesting === 0 ? _log('->', '-') : identity,
+      _log(`->`, '-'),
+      x => {
+        // if (nesting === 0) _log('->', '-')(x)
+        nesting++
+        return x
+      },
+      ...fns
+        .filter(x => typeof x !== 'string')
+        .flatMap(f => {
+          const prefix = f.name + ' =>'
+          padding = Math.max(padding, prefix.length)
+          return [f, _log(prefix)]
+        }),
+      x => {
+        nesting--
+        // if (nesting === 0) _log('=>', '=')(x)
+        return x
+      },
+      _log('=>', '=')
+    )
+  }
+}
+
+export const split = (...args) =>
+  function split(x) {
+    return x.split(...args)
+  }
+
+export const reduce = (...args) =>
+  function reduce(x) {
+    return x.reduce(...args)
+  }
 
 export const _log = (...args) => x => {
   // eslint-disable-next-line no-console
   console.log(...args, x)
   return x
+}
+{
+  _log.out = _log('=>')
+
+  Object.defineProperties(_log, {
+    in: {
+      get() {
+        _log.ix = 0
+        return _log('\n->')
+      },
+    },
+    x: {
+      get() {
+        return _log(++_log.ix + ':')
+      },
+    },
+  })
 }
 
 export const get = steps => route =>
