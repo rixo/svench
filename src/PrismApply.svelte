a3a7ed1bf171a16d5d139b2c41a39506f3128ad0
rixo <rixo@rixo.fr>
2020-06-13 03:03:33 +0200

    prevent leaking .code-toolbar on hmr updates

--- src/PrismApply.svelte
+++ src/PrismApply.svelte
@@ -4,6 +4,11 @@
 
   const highlight = () => {
     if (typeof Prism === 'undefined') return
+    // ensure emptied .code-toolbar are removed (Svelte don't know of them, so
+    // it only cleans the <pre> tag)
+    document.querySelectorAll('.code-toolbar').forEach(x => {
+      if (!x.querySelector('pre')) x.remove()
+    })
     // eslint-disable-next-line no-undef
     Prism.highlightAll()
   }
