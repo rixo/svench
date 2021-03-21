78c42c8d2fd00ae3425768cec2e0ae84cff56aca
rixo <rixo@rixo.fr>
2021-03-21 14:47:14 +0100

    remove unused

--- svench/presets/vite.cjs
+++ svench/presets/vite.cjs
@@ -3,16 +3,6 @@
  */
 
 const path = require('path')
-const os = require('os')
-
-// https://github.com/darkskyapp/string-hash/blob/master/index.js
-// (via https://github.com/sveltejs/svelte/blob/91d758e35b2b2154512ddd11e6b6d9d65708a99e/src/compiler/compile/utils/hash.ts#L2)
-const stringHashcode = str => {
-  let hash = 5381
-  let i = str.length
-  while (i--) hash = ((hash << 5) - hash) ^ str.charCodeAt(i)
-  return (hash >>> 0).toString(36)
-}
 
 // TODO test that we don't eat user's aliases
 const mergeAlias = (...sources) =>
